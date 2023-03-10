44a3cda1a5aa92e8197d4f688b9ccb4c43b56778
Darren Hurst <hurst_darren@hotmail.com>
2023-03-10 18:46:23 -0500

    Initial Commit

---
+++ Sources/Community/Community.swift
@@ -0,0 +1,6 @@
+public struct Community {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/CommunityTests/CommunityTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import Community
+
+final class CommunityTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(Community().text, "Hello, World!")
+    }
+}
